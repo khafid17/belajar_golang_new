ebba214969445434ecbe880cf04d9de43c192231
khafid17 <khafiddurrahman@gmail.com>
2022-03-21 13:43:24 +0700

    compaign

--- handler/user.go
+++ handler/user.go
@@ -133,8 +133,8 @@ func (h *userHandler) UploadAvatar(c *gin.Context) {
 		return
 	}
 
-	//jwt
-	userID := 1
+	currentUser := c.MustGet("currentUser").(user.User)
+	userID := currentUser.ID
 
 	path := fmt.Sprintf("images/%d-%s", userID, file.Filename)
 
