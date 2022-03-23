3350a24fcf990483045740d9db85d7ba4039e32a
khafid17 <khafiddurrahman@gmail.com>
2022-03-23 16:09:15 +0700

    transaction

--- transaction/formatter.go
+++ transaction/formatter.go
@@ -1,1 +1,34 @@
 package transaction
+
+import "time"
+
+type CampaignTransactionFormatter struct {
+	ID        int       `json:"id"`
+	Name      string    `json:"name"`
+	Amount    int       `json:"amount"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
+func FormatCampaigmTransaction(transaction Transaction) CampaignTransactionFormatter {
+	formatter := CampaignTransactionFormatter{}
+	formatter.ID = transaction.ID
+	formatter.Name = transaction.User.Name
+	formatter.Amount = transaction.Amount
+	formatter.CreatedAt = transaction.CreatedAt
+	return formatter
+}
+
+func FormatCampaigmTransactions(transactions []Transaction) []CampaignTransactionFormatter {
+	if len(transactions) == 0 {
+		return []CampaignTransactionFormatter{}
+	}
+
+	var transactionsFormatter []CampaignTransactionFormatter
+
+	for _, transaction := range transactions {
+		formatter := FormatCampaigmTransaction(transaction)
+		transactionsFormatter = append(transactionsFormatter, formatter)
+	}
+
+	return transactionsFormatter
+}
--- transaction/service.go
+++ transaction/service.go
@@ -1,1 +1,36 @@
 package transaction
+
+import (
+	"bwastartup/campaign"
+	"errors"
+)
+
+type service struct {
+	repository Repository
+	campaignRepository campaign.Repository
+}
+
+type Service interface {
+	GetTransactionByCampaignID(input GetCampaignTransactionsInput) ([]Transaction, error)
+}
+
+func NewService(repository Repository, campaignRepository campaign.Repository) *service {
+	return &service{repository, campaignRepository}
+}
+
+func (s *service) GetTransactionByCampaignID(input GetCampaignTransactionsInput) ([]Transaction, error) {
+	campaign, err := s.campaignRepository.FindById(input.ID)
+	if err != nil {
+		return []Transaction{}, err
+	}
+
+	if campaign.UserID != input.User.ID {
+		return []Transaction{}, errors.New("Not an owner of the campaign")
+	}
+	
+	transactions, err := s.repository.GetByCampaignID(input.ID)
+	if err != nil {
+		return transactions, err
+	}
+	return transactions, nil
+}
